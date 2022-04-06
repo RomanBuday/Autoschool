49f89f0b971084c027cb8c5a13c46129c2af76dd
Roman Budai <roman1985buday@gmail.com>
2022-04-06 20:48:15 +0300

    form

--- src/js/main.js
+++ src/js/main.js
@@ -2,3 +2,12 @@ import './_vendor';
 import vars from './_vars';
 import './_functions';
 import './_components';
+
+$(document).ready(function(){
+  $('.instructor__slider').slick({
+    infinite: true,
+    slidesToShow: 2,
+    slidesToScroll: 1
+  });
+});
+
